de0fa5d34bc4a85afae1cbb0fe94fedd7c87af2b
Setty23 <125440661+Setty23@users.noreply.github.com>
2024-01-19 16:40:25 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -2,6 +2,14 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ documment doc """
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary='place_amenity',
+                                       back_populates="amenities")
+    else:
+        name = ""
